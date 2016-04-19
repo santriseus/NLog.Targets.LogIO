cae2489eed1daa63b524d6970643dcc8ffb6f991
andrei.shamanau <andrei.shamanau@softeq.com>
2016-04-19 15:48:32 +0300

    Code cleanup.

--- src/NLog.Targets.LogIO/InternalNetworkTarget.cs
+++ src/NLog.Targets.LogIO/InternalNetworkTarget.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NLog.Common;
+﻿using NLog.Common;
 using NLog.Layouts;
 
 namespace NLog.Targets.LogIO
